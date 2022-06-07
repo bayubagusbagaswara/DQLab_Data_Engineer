829e96b1a56441cc84ab9cdbc85733cab166b3a7
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-07 13:11:39 +0700

    Function Parameter

---
+++ 1-Python Fundamental for Data Science/5-Function/2-parameter-pada-fungsi/FunctionParameter.py
@@ -0,0 +1,8 @@
+# alas dan tinggi merupakan parameter fungsi
+def luas_segitiga(alas, tinggi):
+    luas = (alas * tinggi) / 2
+    print("Luas segitiga: %f" % luas)
+
+
+# Pemanggilan fungsi
+luas_segitiga(4, 6)

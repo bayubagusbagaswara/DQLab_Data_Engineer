c5d4abf84026d737b54705e021b1c4e2d46940e1
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-12 08:35:58 +0700

    Read Dataset SQL

---
+++ 6-Data Manipulation with Pandas - Part 1/2-Dataset IO/4-Read-Dataset-SQL.py
@@ -0,0 +1,3 @@
+# Fungsi .read_sql() atau .read_sql_query() digunakan untuk membaca query dari database dan translate menjadi pandas dataframe, contoh case ini database sqlite.
+
+# Untuk data yang besar(big data), umumnya digunakan Google BigQuery. Layanan ini dapat digunakan jika telah memiliki Google BigQuery account.

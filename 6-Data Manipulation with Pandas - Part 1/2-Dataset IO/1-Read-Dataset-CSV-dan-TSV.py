a8e85cce74dc6dade8a22eff5aaf452249f30644
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-12 09:28:52 +0700

    Indexing

--- 6-Data Manipulation with Pandas - Part 1/2-Dataset IO/1-Read-Dataset-CSV-dan-TSV.py
+++ 6-Data Manipulation with Pandas - Part 1/2-Dataset IO/1-Read-Dataset-CSV-dan-TSV.py
@@ -2,11 +2,10 @@
 
 # Fungsi .read_csv() digunakan untuk membaca file yang value-nya dipisahkan oleh comma (default), terkadang pemisah value-nya bisa di set ‘\t’ untuk file tsv (tab separated values).
 import pandas as pd
-# File CSV
-df_csv = pd.read_csv(
-    "https://storage.googleapis.com/dqlab-dataset/sample_csv.csv")
-print(df_csv.head(3))  # Menampilkan 3 data teratas
-# File TSV
-df_tsv = pd.read_csv(
-    "https://storage.googleapis.com/dqlab-dataset/sample_tsv.tsv", sep='\t')
-print(df_tsv.head(3))  # Menampilkan 3 data teratas
+# Baca file TSV sample_tsv.tsv
+df = pd.read_csv(
+    "https://storage.googleapis.com/dqlab-dataset/sample_tsv.tsv", sep="\t")
+# Index dari df
+print("Index:", df.index)
+# Column dari df
+print("Columns:", df.columns)

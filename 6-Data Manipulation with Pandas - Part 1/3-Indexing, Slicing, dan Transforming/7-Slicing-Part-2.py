7e157e10a78efba97400019b74471a3b97199ecc
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-12 09:41:51 +0700

    Slicing

---
+++ 6-Data Manipulation with Pandas - Part 1/3-Indexing, Slicing, dan Transforming/7-Slicing-Part-2.py
@@ -0,0 +1,5 @@
+# Dalam sub bab sebelumnya telah mempelajari bagaimana melakukan slicing/filtering dataset dengan menggunakan method .loc pada kolom dataset.
+
+# Sekarang, menerapkan berdasarkan index. Tentu syaratnya adalah dataset sudah dilakukan indexing terlebih dahulu melalui penerapan method .set_index
+
+# Baca kembali file TSV "sample_csv.csv" dan set terlebih dahulu indexnya yaitu order_date, order_id, dan product_id. Kemudian slice/filter-lah dataset jika order_date adalah 2019-01-01, order_id adalah 1612339 dan product_id-nya yaitu P2154 dan P2159. Gunakanlah cara pertama.

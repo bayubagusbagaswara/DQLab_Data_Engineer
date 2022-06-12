42e6b42160078606512adc5fcd90575873b31e6b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-12 10:09:40 +0700

    Transforming

--- 6-Data Manipulation with Pandas - Part 1/3-Indexing, Slicing, dan Transforming/7-Slicing-Part-2.py
+++ 6-Data Manipulation with Pandas - Part 1/3-Indexing, Slicing, dan Transforming/7-Slicing-Part-2.py
@@ -3,3 +3,12 @@
 # Sekarang, menerapkan berdasarkan index. Tentu syaratnya adalah dataset sudah dilakukan indexing terlebih dahulu melalui penerapan method .set_index
 
 # Baca kembali file TSV "sample_csv.csv" dan set terlebih dahulu indexnya yaitu order_date, order_id, dan product_id. Kemudian slice/filter-lah dataset jika order_date adalah 2019-01-01, order_id adalah 1612339 dan product_id-nya yaitu P2154 dan P2159. Gunakanlah cara pertama.
+
+import pandas as pd
+# Baca file sample_csv.csv
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/sample_csv.csv")
+# Set index dari df sesuai instruksi
+df = df.set_index(["order_date","order_id","product_id"])
+# Slice sesuai intruksi
+df_slice = df.loc[("2019-01-01",1612339,["P2154","P2159"]),:]
+print("Slice df:\n", df_slice)

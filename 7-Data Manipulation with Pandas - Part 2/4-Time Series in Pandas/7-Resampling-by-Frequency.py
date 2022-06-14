76e03f76e9eb6b4f851bf84a8607d9beef0fc4f1
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-14 14:46:02 +0700

    Resampling by Frequency

---
+++ 7-Data Manipulation with Pandas - Part 2/4-Time Series in Pandas/7-Resampling-by-Frequency.py
@@ -0,0 +1,19 @@
+# [1] Data ini downsampling dari daily to 2 weekly, kemudian dihitung rata-ratanya, jika ada nilai NaN maka dapat diisi dengan fillna method = 'ffill'
+
+# [2] Selanjutnya, data awal di upsampling dari daily to 8 hourly, kemudian hitung rata-ratanya, jika ada nilai NaN maka dapat di isi dengan fillna method = 'bfill'
+
+# Tugas Praktik:
+
+# Kerjakanlah proses data awal resampling dari daily to bi-monthly, kemudian hitung rata-ratanya, jika ada nilai NaN maka dapat diisi dengan fillna method = 'bfill' serta mengisi kode yang tidak lengkap (_ _ _) di code editor.
+
+import pandas as pd
+# Load dataset https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv(
+    'https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv')
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+print('Dataset sebelum di-resampling (5 teratas):\n', gaq.head())
+# Resample dari daily to 2 monthly, hitung reratanya, dan fillna = 'bfill'
+gaq_2monthly = gaq.resample('2M').mean().fillna(method='bfill')
+print('Resampling daily to 2 monthly - mean - ffill (5 teratas):\n',
+      gaq_2monthly.head())

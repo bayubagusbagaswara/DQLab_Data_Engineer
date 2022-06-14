21b4674a7a16f942f7d5b8c9c09960accdbd1644
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-14 10:58:00 +0700

    Concat

--- 7-Data Manipulation with Pandas - Part 2/1-Penggabungan Series atau DataFrame/3-Concat.py
+++ 7-Data Manipulation with Pandas - Part 2/1-Penggabungan Series atau DataFrame/3-Concat.py
@@ -7,11 +7,11 @@ df1 = pd.DataFrame({'a': [1, 2],
 df2 = pd.DataFrame({'b': [1, 2],
                     'a': [3, 4]})
 # Terapkan method concat row-wise
-row_wise_concat = pd.concat([df1, df2])
+row_wise_concat = pd.concat([df2, df1])
 print("Row-wise - concat:\n", row_wise_concat)
 # Terapkan method concat column-wise
-col_wise_concat = pd.concat([df1, df2], axis=1)
+col_wise_concat = pd.concat([df2, df1], axis=1)
 print("Column-wise - concat:\n", col_wise_concat)
 # Penambahan identifier --> membentuk hasil penggabungan multiindex
-multiindex_concat = pd.concat([df1, df2], axis=0, keys=['df1', 'df2'])
+multiindex_concat = pd.concat([df2, df1], axis=0, keys=['df2', 'df1'])
 print("Multiindex - concat:\n", multiindex_concat)

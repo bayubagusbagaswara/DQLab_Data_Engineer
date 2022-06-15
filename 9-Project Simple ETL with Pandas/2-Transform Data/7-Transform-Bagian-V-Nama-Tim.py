fada07ea67c60114c1b3f0ab97e461f0c74952ed
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-15 10:50:21 +0700

    Transform Bagian V - Nama Tim

---
+++ 9-Project Simple ETL with Pandas/2-Transform Data/7-Transform-Bagian-V-Nama-Tim.py
@@ -0,0 +1,36 @@
+# Dataset saat ini belum memuat nama tim, dan rupanya dari tim Data Analyst membutuhkan informasi terkait nama tim dari masing-masing peserta.
+
+# Diketahui bahwa nama tim merupakan gabungan nilai dari kolom first_name, last_name, country dan institute.
+
+# Tugas Anda yakni buatlah kolom baru dengan nama team_name yang memuat informasi nama tim dari peserta.
+
+
+import pandas as pd
+df_participant = pd.read_csv(
+    'https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+df_participant['postal_code'] = df_participant['address'].str.extract(
+    r'(\d+)$')
+df_participant['city'] = df_participant['address'].str.extract(
+    r'(?<=\n)(\w.+)(?=,)')
+df_participant['github_profile'] = 'https://github.com/' + \
+    df_participant['first_name'].str.lower(
+) + df_participant['last_name'].str.lower()
+df_participant['cleaned_phone_number'] = df_participant['phone_number'].str.replace(
+    r'^(\+62|62)', '0')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(
+    r'[()-]', '')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(
+    r'\s+', '')
+
+
+def func(col):
+    # Singkatan dari Nama Depan dan Nama Belakang dengan mengambil huruf pertama
+    abbrev_name = "%s%s" % (col['first_name'][0], col['last_name'][0])
+    country = col['country']
+    # Singkatan dari value di kolom institute
+    abbrev_institute = '%s' % (
+        ''.join(list(map(lambda word: word[0], col['institute'].split()))))
+    return "%s-%s-%s" % (abbrev_name, country, abbrev_institute)
+
+
+df_participant['team_name'] = df_participant.apply(func, axis=1)

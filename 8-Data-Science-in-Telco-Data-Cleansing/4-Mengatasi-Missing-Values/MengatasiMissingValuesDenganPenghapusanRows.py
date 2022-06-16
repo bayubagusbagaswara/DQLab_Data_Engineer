d118174e35fb1be985798ebeb8e9b4e2b52a0da1
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-16 22:42:19 +0700

    Mengatasi Missing Values dengan Penghapusan Rows

--- 8-Data-Science-in-Telco-Data-Cleansing/4-Mengatasi-Missing-Values/MengatasiMissingValuesDenganPenghapusanRows.py
+++ 8-Data-Science-in-Telco-Data-Cleansing/4-Mengatasi-Missing-Values/MengatasiMissingValuesDenganPenghapusanRows.py
@@ -1,11 +1,3 @@
-# Selanjutnya kita akan menghapus rows dari data-data yang tidak terdeteksi apakah dia churn atau tidak. Diasumsikan data modeller hanya mau menerima data yang benar ada flag churn-nya atau tidak.
-
-# Gunakan isnull() digunakan untuk mendeteksi missing values dan dropna() untuk menghapus data yang missing values.
-
-# Berikut adalah hasil yang diharapkan:
-# Total missing values data dari kolom Churn 43
-# Total Rows dan kolom Data setelah dihapus data Missing Values adalah(6950, 22)
-
 import pandas as pd
 df_load = pd.read_csv(
     'https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
@@ -19,6 +11,7 @@ df_load = df_load.sort_values(
 
 print('Total missing values data dari kolom Churn',
       df_load['Churn'].isnull().sum())
+
 # Dropping all Rows with spesific column (churn)
 df_load.dropna(subset=['Churn'], inplace=True)
 print('Total Rows dan kolom Data setelah dihapus data Missing Values adalah', df_load.shape)

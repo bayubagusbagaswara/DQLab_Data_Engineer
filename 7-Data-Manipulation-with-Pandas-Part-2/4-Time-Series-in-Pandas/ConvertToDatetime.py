85ab272b351e192628ed26b655f530f6d6ee3b6f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-16 22:18:52 +0700

    Convert to Datetime

---
+++ 7-Data-Manipulation-with-Pandas-Part-2/4-Time-Series-in-Pandas/ConvertToDatetime.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Load dataset https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv(
+    'https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv')
+
+# Cetak 5 data teratasgit
+print('Sebelum diubah dalam format datetime:\n', gaq.head())
+
+# Ubah menjadi datetime
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+
+# Cetak 5 data teratas
+print('Sesudah diubah dalam format datetime:\n', gaq.head())

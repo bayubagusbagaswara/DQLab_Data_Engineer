85f629aa5cb40f997feb00c2121aaa96f428bc42
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-16 22:06:29 +0700

    Melt Part 1

--- 7-Data-Manipulation-with-Pandas-Part-2/2-Pivot-Melt-Stack-Unstack/MeltPart1.py
+++ 7-Data-Manipulation-with-Pandas-Part-2/2-Pivot-Melt-Stack-Unstack/MeltPart1.py
@@ -1,12 +1,5 @@
-# Teknik melt melalui pd.melt() digunakan untuk mengembalikan kondisi data yang sudah dilakukan pivot menjadi sebelum pivot.
-
-# Mari diperhatikan kembali dataframe yang telah digunakan sebelumnya dan dataframenya sudah di pivot.
-
-
-# [1] Melting dataframe
-
-# [2] Dengan menspesifikasi keyword argument id_vars yang ditujukan untuk membuat fix kolom yang sebagai id tiap barisnya.
 import pandas as pd
+
 # Dataframe
 data = pd.DataFrame({
     'kelas': 6*['A'] + 6*['B'],
@@ -14,13 +7,16 @@ data = pd.DataFrame({
     'pelajaran': 6*['math', 'english'],
     'nilai': [90, 60, 70, 85, 50, 60, 100, 40, 95, 80, 60, 45]
 }, columns=['kelas', 'murid', 'pelajaran', 'nilai'])
+
 # Pivoting dataframe
 data_pivot = data.pivot_table(
     index='kelas', columns='pelajaran', values='nilai', aggfunc='mean').reset_index()
 print('Pivoting dataframe:\n', data_pivot)
+
 # [1] Melting dataframe data_pivot
 data_melt_1 = pd.melt(data_pivot)
 print('Melting dataframe:\n', data_melt_1)
+
 # [2] Melting dataframe data_pivot dengan id_vars
 data_melt_2 = pd.melt(data_pivot, id_vars='kelas')
 print('Melting dataframe dengan idvars:\n', data_melt_2)

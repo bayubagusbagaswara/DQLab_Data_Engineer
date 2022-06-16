1a9f9e286e624c394d4958c5656ff8ea07e365cb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-16 22:05:46 +0700

    Pivot Table

--- 7-Data-Manipulation-with-Pandas-Part-2/2-Pivot-Melt-Stack-Unstack/PivotTable.py
+++ 7-Data-Manipulation-with-Pandas-Part-2/2-Pivot-Melt-Stack-Unstack/PivotTable.py
@@ -1,12 +1,5 @@
-# Apa yang terjadi kalau output pivot tabel memiliki duplicate index? Seperti yang diketahui, index di dataframe adalah unique identifier untuk setiap row, jadi tidak boleh ada duplikat dan setiap membuat pivot tabel, harus specify index as kolom yang mana dan columns-nya memakai kolom yang mana.
-
-# Hal ini dapat diatasi dengan melakukan method .pivot_table() pada dataframe. Metode ini sama seperti melakukan pivot pada tabel tapi juga melakukan groupby dan aggregation (aggfunc) pada level rows sehingga dipastikan tidak ada duplicate index di rows (secara default aggfunc = 'mean').
-
-# Tugas Praktek:
-
-# Seperti yang dicontohkan untuk meng-create pivot tabel dengan method .pivot_table() tetapi aggfunc yang digunakan adalah 'mean' dan 'median'.
-
 import pandas as pd
+
 # Dataframe
 data = pd.DataFrame({
     'kelas': 6*['A'] + 6*['B'],
@@ -14,10 +7,12 @@ data = pd.DataFrame({
     'pelajaran': 6*['math', 'english'],
     'nilai': [90, 60, 70, 85, 50, 60, 100, 40, 95, 80, 60, 45]
 }, columns=['kelas', 'murid', 'pelajaran', 'nilai'])
+
 # Creating pivot and assign pivot_tab dengan menggunakan keyword aggfunc='mean'
 pivot_tab_mean = data.pivot_table(
     index='kelas', columns='pelajaran', values='nilai', aggfunc='mean')
 print('Creating pivot table -- aggfunc mean:\n', pivot_tab_mean)
+
 # Creating pivot and assign pivot_tab dengan menggunakan keyword aggfunc='median'
 pivot_tab_median = data.pivot_table(
     index='kelas', columns='pelajaran', values='nilai', aggfunc='median')

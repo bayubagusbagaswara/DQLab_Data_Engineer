1de60515ee41fe008eac1b2b39394bc1ff6d9bc4
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-16 22:08:39 +0700

    Stack and Unstack Part 2

--- 7-Data-Manipulation-with-Pandas-Part-2/2-Pivot-Melt-Stack-Unstack/StackAndUnstackPart2.py
+++ 7-Data-Manipulation-with-Pandas-Part-2/2-Pivot-Melt-Stack-Unstack/StackAndUnstackPart2.py
@@ -1,14 +1,5 @@
-# Dalam bagian kedua dari Stack & Unstack ini akan membahas stacking dataframe.
-
-# [1] Stacking dataframe
-# [2] Tukar posisi index setelah stacking dataframe
-# [3] Melakukan sort_index pada stacking dataframe
-
-# Tugas Praktik:
-
-# Kerjakanlah di code editor dengan jalan mengisi kode yang tidak lengkap (_ _ _) sesuai dengan yang telah dicontohkan.
-
 import pandas as pd
+
 # Dataframe
 data = pd.DataFrame({
     'kelas': 6*['A'] + 6*['B'],
@@ -16,15 +7,19 @@ data = pd.DataFrame({
     'pelajaran': 6*['math', 'english'],
     'nilai': [90, 60, 70, 85, 50, 60, 100, 40, 95, 80, 60, 45]
 }, columns=['kelas', 'murid', 'pelajaran', 'nilai'])
+
 data = data.set_index(['kelas', 'murid', 'pelajaran'])
 data_unstack = data.unstack(level=1)
 print('Dataframe:\n', data_unstack)
+
 # [1] Stacking dataframe
 data_stack = data_unstack.stack()
 print('Stacked dataframe:\n', data_stack)
+
 # [2] Tukar posisi index setelah stacking dataframe
 data_swap = data_stack.swaplevel(1, 2)
 print('Swapped data:\n', data_swap)
+
 # [3] Melakukan sort_index pada stacking dataframe
 data_sort = data_stack.sort_index()
 print('Sorted data:\n', data_sort)

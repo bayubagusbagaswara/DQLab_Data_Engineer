b36049b129c201739971fe7795a4ad393ca401f2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-16 22:23:44 +0700

    Visualisasi

--- 7-Data-Manipulation-with-Pandas-Part-2/4-Time-Series-in-Pandas/Visualisasi.py
+++ 7-Data-Manipulation-with-Pandas-Part-2/4-Time-Series-in-Pandas/Visualisasi.py
@@ -1,16 +1,12 @@
-# Selanjutnya untuk memvisualisasikan dengan menggunakan library matplotlib dataframe yang telah diolah (seperti yang telah dipelajari dari bab pertama hingga subbab sebelumnya di bab ini).
-# Terdapat beberapa plot styles yang dapat diterapkan dalam pembuatan visualisasi di matplotlib.
-# Visualisasi tentang average jumlah masing-masing polutan berdasarkan bulan waktunya.
-
-# [1] Membuat pivot table yang menunjukkan waktu di barisnya dan masing-masing value dari pollutant-nya dalam kolom.
-# [2] Membuat fungsi yang memberikan default value 0 ketika value-nya di bawah 0 dan apply ke setiap elemen dari dataset tersebut, kemudian menampilkannya sebagai chart.
 import pandas as pd
 import matplotlib.pyplot as plt
+
 # Load dataset https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
 gaq = pd.read_csv(
     'https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv')
 gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
 gaq = gaq.set_index('timestamp')
+
 # [1] Membuat pivot table yang menunjukkan waktu di baris nya dan masing-masing value dari pollutant nya dalam kolom
 gaq_viz = gaq[['pollutant', 'value']].reset_index(
 ).set_index(['timestamp', 'pollutant'])
@@ -18,6 +14,7 @@ gaq_viz = gaq_viz.pivot_table(
     index='timestamp', columns='pollutant', aggfunc='mean').fillna(0)
 gaq_viz.columns = gaq_viz.columns.droplevel(0)
 print('Data (5 teratas):\n', gaq_viz.head())
+
 # [2] Membuat fungsi yang memberikan default value 0 ketika value nya di bawah 0 dan apply ke setiap elemen dari dataset tersebut, kemudian menampilkannya sebagai chart
 
 

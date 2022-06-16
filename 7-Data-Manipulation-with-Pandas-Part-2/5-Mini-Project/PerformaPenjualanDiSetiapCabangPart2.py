49d645273f15b7ca280abcbdadc576a6dda17ad9
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-16 22:26:59 +0700

    Performa Penjualan di Setiap Cabang Part 2

--- 7-Data-Manipulation-with-Pandas-Part-2/5-Mini-Project/PerformaPenjualanDiSetiapCabangPart2.py
+++ 7-Data-Manipulation-with-Pandas-Part-2/5-Mini-Project/PerformaPenjualanDiSetiapCabangPart2.py
@@ -1,15 +1,3 @@
-# Melanjutkan penyelesaian persoalan ke langkah berikutnya.
-
-# [3] Transformasi Data
-
-# Jika ada data yang tidak seharusnya maka dapat dibuang
-# Jika ada kolom yang seharusnya bertipe datetime64 ubahlah
-# Cek kembali informasi dataframe
-# Tampilkan kembali statistik deskriptif dari dataframe
-# Untuk representasi string gunakan kutip satu, misalnya: 'contoh string'.
-
-# Sebagai kontrol, informasi terakhir (baris 32) berikut dengan statistik deskriptif (baris 34) adalah:
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -27,27 +15,34 @@ retail_table = pd.concat(
 
 # [3]. Transformasi Data
 print('TRANSFORMASI DATA\n\n')
+
 #      Memastikan data yang memiliki item_price < 0 atau total_price < 0
 cek = retail_table.loc[(retail_table['item_price'] < 0)
                        | (retail_table['total_price'] < 0)]
 print('\nitem_price < 0 atau total_price < 0:\n', cek)
+
 #      Jika tidak masuk akal datanya dapat dibuang
 if cek.shape[0] != 0:
     retail_table = retail_table.loc[(retail_table['item_price'] > 0) & (
         retail_table['total_price'] > 0)]
+
 #      Cek apakah masih ada order_id yang bernilai undefined dan delete row tersebut
 cek = retail_table.loc[retail_table['order_id'] == 'undefined']
 print('\norder_id yang bernilai undefined:\n', cek)
+
 #      Jika ada maka buang baris tersebut
 if cek.shape[0] != 0:
     retail_table = retail_table.loc[retail_table['order_id'] != 'undefined']
 
 #      Transform order_id menjadi int64
 retail_table['order_id'] = retail_table['order_id'].astype('int64')
+
 #      Transform order_date menjadi datetime Pandas
 retail_table['order_date'] = pd.to_datetime(retail_table['order_date'])
+
 #      Cek dataframe info kembali untuk memastikan
 print('\nInfo:')
 print(retail_table.info())
+
 #      Cek statistik deskriptif kembali, untuk memastikan
 print('\nStatistik deskriptif:\n', retail_table.describe())

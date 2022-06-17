de0a878623e90800606cd1ae4155f123e4917c6f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-17 13:13:51 +0700

    Atribut DataFrame dan Series Part 1

--- 6-Data-Manipulation-with-Pandas-Part-1/1-Introduction-to-Pandas/AtributDataFrameDanSeriesPart1.py
+++ 6-Data-Manipulation-with-Pandas-Part-1/1-Introduction-to-Pandas/AtributDataFrameDanSeriesPart1.py
@@ -1,23 +1,28 @@
-# Dataframe dan Series memiliki sangat banyak atribut yang digunakan untuk transformasi data, tetapi ada beberapa attribute yang sering dipakai. Di sini series number_list dan dataframe matrix_list pada subbab sebelumnya digunakan kembali.
 import pandas as pd
+
 # Series
 number_list = pd.Series([1, 2, 3, 4, 5, 6])
+
 # DataFrame
 matrix_list = pd.DataFrame([[1, 2, 3],
                             ['a', 'b', 'c'],
                             [3, 4, 5],
                             ['d', 4, 6]])
+
 # [1] method .info()
 print("[1] method .info()")
 print(matrix_list.info())
+
 # [2] attribute .shape
 print("\n[2] attribute .shape")
 print("    Shape dari number_list:", number_list.shape)
 print("    Shape dari matrix_list:", matrix_list.shape)
+
 # [3] attribute .dtypes
 print("\n[3] attribute .dtypes")
 print("    Tipe data number_list:", number_list.dtypes)
 print("    Tipe data matrix_list:", matrix_list.dtypes)
+
 # [4] attribute .astype()
 print("\n[4] method .astype()")
 print("    Konversi number_list ke str:", number_list.astype("str"))

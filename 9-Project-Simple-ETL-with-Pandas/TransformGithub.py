2039aa030e75b2b19be463ed2cc9ded7cef41044
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-17 08:41:59 +0700

    Transform Github

--- 9-Project-Simple-ETL-with-Pandas/TransformGithub.py
+++ 9-Project-Simple-ETL-with-Pandas/TransformGithub.py
@@ -1,9 +1,3 @@
-# Salah satu parameter untuk mengetahui proyek apa saja yang pernah dikerjakan oleh peserta yaitu dari git repository mereka.
-
-# Pada kasus ini kita menggunakan profil github sebagai parameternya. Tugas Anda yaitu membuat kolom baru bernama github_profile yang merupakan link profil github dari peserta.
-
-# Diketahui bahwa profil github mereka merupakan gabungan dari first_name dan last_name yang sudah di-lowercase.
-
 import pandas as pd
 df_participant = pd.read_csv(
     'https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')

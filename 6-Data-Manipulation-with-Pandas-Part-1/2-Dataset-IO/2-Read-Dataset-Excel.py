7f94e6548208450dd1af993b02ce7927d9b5aedd
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-17 13:21:22 +0700

    Read Dataset Excel

--- 6-Data-Manipulation-with-Pandas-Part-1/2-Dataset-IO/2-Read-Dataset-Excel.py
+++
@@ -1,8 +0,0 @@
-# File Excel dengan ekstensi *.xls atau *.xlsx cukup banyak digunakan dalam menyimpan data. Pandas juga memiliki fitur untuk membaca file excel.
-# Fungsi .read_excel() digunakan untuk membaca file excel menjadi dataframe pandas.
-
-import pandas as pd
-# File xlsx dengan data di sheet "test"
-df_excel = pd.read_excel(
-    "https://storage.googleapis.com/dqlab-dataset/sample_excel.xlsx", sheet_name="test")
-print(df_excel.head(4))  # Menampilkan 4 data teratas
